b5cdb79770d1a8e7656e71c96f91ffdf49b05eb9
phonglh <phonghngle@gmail.com>
2025-06-06 00:43:18 +0700

    style: adjust text color and font sizes in blog post layout

--- src/components/home/Posts.tsx
+++ src/components/home/Posts.tsx
@@ -13,15 +13,15 @@ export default async function Posts({locale}: PostProps) {
   return (
     <div className="space-y-12">
       <div className="space-y-4">
-        <h3 className="text-2xl font-instrument-serif">
+        <h3 className="text-2xl">
           <em>Journey</em> Diary
         </h3>
-        <p className="text-subtle">The things I think</p>
+        <p className="text-primary/50">The things I think</p>
         <PostHighlight posts={content} />
       </div>
 
       <div className="space-y-4">
-        <h3 className="text-2xl font-instrument-serif">All Posts</h3>
+        <h3 className="text-2xl">All Posts</h3>
         <PostLatest posts={allPosts} />
       </div>
     </div>
