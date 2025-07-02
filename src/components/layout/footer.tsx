4c183c9bed811165f3fd3b8956a100b30823e1d7
phonglh <phonghngle@gmail.com>
2025-07-02 20:59:17 +0700

    update footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import { useTranslations } from '@stackhub/i18n/client';
+import { Link } from '@/stackhub/ui';
 
 const Footer = () => {
-  const t = useTranslations();
-
   return (
-    <footer className="bg-background/30 mx-auto mt-12 mb-6 w-full max-w-6xl rounded-2xl p-8 backdrop-blur-md">
-      <div className="mt-12 flex flex-col items-center justify-center gap-4 border-t pt-6 text-sm sm:flex-row">
+    <footer className="bg-background/30 mx-auto mt-12 mb-6 w-full max-w-6xl rounded-2xl backdrop-blur-md">
+      <div className="mt-12 flex flex-col items-center justify-between gap-4 border-t pt-6 text-sm sm:flex-row">
         <span className="text-muted-foreground">
-          &copy; {new Date().getFullYear()} Henry Le. All rights reserved.
+          &copy; {new Date().getFullYear()} phnglh.
         </span>
+        <div className="flex gap-4 text-muted-foreground">
+          <Link href="https://github.com/phnglh">GitHub</Link>
+          <Link href="https://linkedin.com/in/hongphongle/">LinkedIn</Link>
+        </div>
       </div>
     </footer>
   );
